e3fc18ad45708ea8f2ff0d26b35450cd4366e839
Tim Makhel <tmaxell@MacBook-Pro.local>
2024-10-24 02:08:21 +0300

    -f, -s and -i flag proccessing and some fixes in input

--- hw3/uniq.go
+++ hw3/uniq.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
+	"os"
+	"strings"
 )
 
 func main() {
@@ -17,5 +20,49 @@ func main() {
 
 	if *countFlag && (*doubleFlag || *uniqueFlag) || *doubleFlag && *uniqueFlag {
 		fmt.Println("Ошибка: нельзя одновременно использовать флаги -c, -d, -u")
+		flag.Usage()
+		os.Exit(1)
+	}
+	// тут будем читать файлик
+	var input *os.File
+	var err error
+	if flag.NArg() > 0 {
+		input, err = os.Open(flag.Arg(0))
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Ошибка отркытия файла: %v\n", err)
+			os.Exit(1)
+		}
+		defer input.Close()
+	} else {
+		input = os.Stdin
+	}
+	var output *os.File
+	if flag.NArg() > 1 {
+		output, err = os.Create(flag.Arg(1))
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Ошибка создания файла: %v\n", err)
+			os.Exit(1)
+		}
+		defer output.Close()
+	} else {
+		output = os.Stdout
+	}
+
+	scanner := bufio.NewScanner(input)
+	linesCount := make(map[string]int)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		if *ignoreFlag {
+			line = strings.ToLower(line)
+		}
+		line = processLine(line, *numFieldsFlag, *numCharsFlag)
+		if line != "" {
+			lineCount[line]++
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintf(os.Stderr, "Ошибка чтения: %v\n", err)
+		os.Exit(1)
 	}
 }
